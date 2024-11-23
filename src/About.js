5f8320aa9b018d08ff57beb2adcc9c87b21b7aed
sumith006 <kolekarsumith@gmail.com>
2024-11-23 11:33:29 +0530

    abt'

---
+++ src/About.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About page here!</h1>
+            <p>
+                How are you?
+            </p>
+        </div>
+    );
+};
+export default About; 
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom"; function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my react app!</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,pariatur?
+      </p>
+      <br />
+      <Link to="/about">About Page</Link>
+    </div>
+  );
+}
+export default App;
